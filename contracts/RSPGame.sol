5b371cc0734285589599e28e6d7d5e4612742c17
wilvang <johan@wilvang.com>
2025-02-14 12:56:58 +0100

    Created modifiers to the functions to make the code more readable.

--- contracts/RSPGame.sol
+++ contracts/RSPGame.sol
@@ -23,7 +23,6 @@ contract RPSGame {
         uint deadline;       // The deadline to reveal the vote.
     }
 
-   
     // Events for EVM logging.
     event HiddenVote(address _player, bytes32 _hashedVote);
     event RevealedVote(address _player, string _vote);
@@ -37,7 +36,6 @@ contract RPSGame {
     uint public stake = 1 ether;   // Stake per player.
     uint public revealTime = 480;  // 8 minutes in seconds.
 
-
     /**
      * @notice The vote must be a valid character ('R', 'P', 'S')
      *         and be concatenated with salt. The hashing
@@ -46,13 +44,7 @@ contract RPSGame {
      * @dev Allows a player to join the game with a hashed vote.
      * @param _hashedVote The hashed vote of the player.
      */
-    function play(bytes32 _hashedVote) public payable  {
-
-        // Checks if the player already is in a game.
-        require(players[msg.sender].hashedVote == 0, "You are already playing");
-
-         // Checks if the player has paid the stake requirement.
-        require(msg.value >= stake, "The stake must be 1 Ether");
+    function play(bytes32 _hashedVote) public payable isNewPlayer costs(stake) {
 
         // Creates a new player.
         players[msg.sender] = Player({
@@ -99,14 +91,7 @@ contract RPSGame {
      * @param _vote The vote of the player.
      * @param _salt The salt used to hash the vote.
      */
-    function reveal(string calldata _vote, string calldata _salt) public {
-
-        // Checks if the player has an opponent.
-        require(players[msg.sender].opponent != address(0), "Waiting for an opponent");
-
-        // Checks the integrity of the revealed vote.
-        bytes32 hash = keccak256(abi.encodePacked(_vote, _salt));
-        require(players[msg.sender].hashedVote == hash, "Not a valid vote");
+    function reveal(string calldata _vote, string calldata _salt) public hasOpponent validVote(_vote, _salt) {
 
         bytes memory bStr = bytes(_vote); // Byte value of the character.
 
@@ -128,17 +113,7 @@ contract RPSGame {
      * @notice You can only withdraw on victory, tie, or timeout of the deadline.
      * @dev Allows a player to withdraw their stake if conditions are met.
      */
-    function withdraw() public payable  {
-        // Checks if the player has revealed their vote.
-        require(players[msg.sender].revealed, "You must reveal your vote before withdrawal");
-
-        // Checks if the deadline is reached or the
-        // other player has revealed their vote.
-        require(
-            block.timestamp >= players[msg.sender].deadline ||
-            players[players[msg.sender].opponent].revealed,
-            "Waiting for opponent to reveal or deadline"
-        );
+    function withdraw() public payable canWithdraw {
 
         address opponent = players[msg.sender].opponent; // The opponent's address.
         string memory vote1 = players[msg.sender].vote;  // The player's vote.
@@ -185,16 +160,80 @@ contract RPSGame {
     }
 
     /**
+     * @dev Modifier to check if the player is new.
+     */
+    modifier isNewPlayer() {
+        // Checks if the player already is in a game.
+        require(players[msg.sender].hashedVote == 0, "You are already playing");
+        _;
+    }
+
+    /**
+     * @dev Modifier to check if the player has paid the stake requirement.
+     * @param _amount The required stake amount.
+     */
+    modifier costs(uint _amount) {
+        // Checks if the player has paid the stake requirement.
+        require(msg.value >= stake, "The stake must be 1 Ether");
+        _;
+    }
+
+    /**
+     * @dev Modifier to check if the player has an opponent.
+     */
+    modifier hasOpponent() {
+        // Checks if the player has an opponent.
+        require(players[msg.sender].opponent != address(0), "Waiting for an opponent");
+        _;
+    }
+
+    /**
+     * @dev Modifier to check if the player can withdraw.
+     */
+    modifier canWithdraw() {
+        // Checks if the player has revealed their vote.
+        require(players[msg.sender].revealed, "You must reveal your vote before withdrawal");
+
+        // Checks if the deadline is reached or the
+        // other player has revealed their vote.
+        require(
+            block.timestamp >= players[msg.sender].deadline ||
+            players[players[msg.sender].opponent].revealed,
+            "Waiting for opponent to reveal or deadline"
+        );
+        _;
+    }
+
+    /**
+     * @dev Modifier to check the integrity of the revealed vote.
+     * @param _vote The revealed vote.
+     * @param _salt The salt used to hash the vote.
+     */
+    modifier validVote(string memory _vote, string memory _salt) {
+        // Checks the integrity of the revealed vote.
+        bytes32 hash = keccak256(abi.encodePacked(_vote, _salt));
+        require(players[msg.sender].hashedVote == hash, "Not a valid vote");
+        _;
+    }
+
+    /**
+     * @dev Modifier to check if the vote is a valid choice.
+     * @param _vote The vote to check.
+     */
+    modifier validChoice(string memory _vote) {
+        // Checks if the vote is a valid choice.
+        require(compareStrings(_vote, "R") || compareStrings(_vote, "P") || compareStrings(_vote, "S"),
+        "You did not choose rock, paper or scissors");
+        _;
+    }
+
+    /**
      * @dev Determines if the first vote is the winner.
      * @param _vote1 The first vote.
      * @param _vote2 The second vote.
      * @return True if the first vote is the winner, false otherwise.
      */
-    function isWinner(string memory _vote1, string memory _vote2) public pure returns(bool) {
-        // Checks if the vote is a valid choice.
-        require(compareStrings(_vote1, "R") || compareStrings(_vote1, "P") || compareStrings(_vote1, "S"),
-        "You did not choose rock, paper or scissors");
-        
+    function isWinner(string memory _vote1, string memory _vote2) public pure validChoice(_vote1) returns(bool) {
         // Both players chose the same option.
         if (compareStrings(_vote1, _vote2) || compareStrings(_vote2, "")) return false;
         // Paper beats rock
