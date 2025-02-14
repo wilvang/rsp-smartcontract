f477d9f70f06cd5f8a9bd69b5a9102f5de33077f
wilvang <johan@wilvang.com>
2025-02-14 13:29:25 +0100

    Added refund for payments larger than the stake.

--- contracts/RSPGame.sol
+++ contracts/RSPGame.sol
@@ -8,6 +8,8 @@ pragma solidity >=0.7.0 <0.9.0;
  * @author Johan F. Wilvang
  * @notice This contract allows players to participate in a Rock-Paper-Scissors game with a stake.
  *         Players submit their hashed votes and later reveal them to determine the winner.
+ *         The gas fee for two player finishing a normal game where no unusual protocol violations
+ *.        happen is: 428 555 gas.
  * @dev The contract handles player registration, vote submission, vote revealing, and stake withdrawal.
  */
 contract RPSGame {
@@ -77,6 +79,9 @@ contract RPSGame {
             playerQueue.pop();
             emit GameStart(opponent, msg.sender, deadline);
         }
+
+        // Refunds the overshooting amount to the player
+        payable(msg.sender).transfer(msg.value - stake);
 
         // Logs the hidden vote to the player.
         emit HiddenVote(msg.sender, _hashedVote);
